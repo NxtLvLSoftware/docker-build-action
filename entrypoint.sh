55186807ca4b34dcc1a7f015c481736f1dd13b0a
JackNoordhuis <jackjnoordhuis@gmail.com>
2020-07-05 18:50:49 +1000

    attempt to fix action #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-if [ -z "${INPUT_REGISTRY_USERNAME}" ] || ["${INPUT_REGISTRY_USERNAME}" == ""]; then
+if [ -z "${INPUT_REGISTRY_USERNAME}" ] || [ "${INPUT_REGISTRY_USERNAME}" == "" ]; then
 	echo "Username is empty. Please set the username input to login to the docker registry."
 fi
 
-if [ -z "${INPUT_REGISTRY_PASSWORD}" ] || ["${INPUT_REGISTRY_PASSWORD}" == ""]; then
+if [ -z "${INPUT_REGISTRY_PASSWORD}" ] || [ "${INPUT_REGISTRY_PASSWORD}" == "" ]; then
 	echo "Password is empty. Please set the password input to login to the docker registry."
 fi
 
-if [ -z "${INPUT_REGISTRY_URI}" ] || ["${INPUT_REGISTRY_URI}" == ""]; then
+if [ -z "${INPUT_REGISTRY_URI}" ] || [ "${INPUT_REGISTRY_URI}" == "" ]; then
 	echo "${INPUT_REGISTRY_PASSWORD}" | docker login -u ${INPUT_REGISTRY_USERNAME} --password-stdin
 else
 	echo "${INPUT_REGISTRY_PASSWORD}" | docker login ${INPUT_REGISTRY_URI} -u ${INPUT_REGISTRY_USERNAME} --password-stdin
